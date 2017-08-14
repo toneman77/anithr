aca13fbf0c186bb2cb3263e479fb2bdb74ef52cc
Toni <jeder@mail1a.de>
2017-08-14 22:16:09 +0200

    better comment

--- getTop25.sh
+++ getTop25.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 #gives us USERID, PASSW and KONGURL
 source $HOME/scripts/kong.auth
+# rumble number + 50000, so this is rumble number 12
 curl -s "${KONGURL}getRankings&ranking_id=event&ranking_index=50012" --compressed -o /tmp/rumbleranks.json
 echo "rank,name,points,factionid"
 jq -r '.rankings.data[] | [.rank, .name, .stat, .faction_id] | @csv' /tmp/rumranks.json
